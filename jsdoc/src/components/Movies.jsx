eb75f063611750e002fbb201c13f79d9068e25f5
Felipe Merchan <felipemerchanfuquen@gmail.com>
2024-08-28 17:24:51 -0500

    Casos de uso de JSDoc. Caso de uso IV

--- jsdoc/src/components/Movies.jsx
+++ jsdoc/src/components/Movies.jsx
@@ -1,3 +1,5 @@
+import React from 'react'
+
 export function ListOfMovies({ movies }) {
   return (
     <ul className='movies'>
@@ -27,5 +29,35 @@ export function Movies({ movies }) {
     hasMovies
      ? <ListOfMovies movies={movies} />
      : <NoMoviesResults />
+  )
+}
+
+/**
+ * Componente de boton reutilizable
+ * @function
+ * @param {object} props - Las props para el componente
+ * @param {React.ReactNode} props.children - Contenido interno que va dentro del botón
+ * @returns {JSX.Element}
+ */
+export function Button({ children }) {
+  return (
+    <button>{children}</button>
+  )
+}
+
+/**
+ * Componente de encabezado reutilizable
+ * @function
+ * @param {object} props - Las props para el componente
+ * @param {string} props.title - El título del encabezado
+ * @param {string} [props.subtitle] - El subtítulo opcional del encabezado
+ * @returns {JSX.Element} El componente de encabezado
+ */
+export function Header({ title, subtitle }) {
+  return (
+    <header>
+      <h1>{title}</h1>
+      {subtitle && <h2>{subtitle}</h2>}
+    </header>
   )
 }
