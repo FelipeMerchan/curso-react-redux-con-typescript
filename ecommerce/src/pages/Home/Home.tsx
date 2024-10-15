b3774d0edf696e220a69f0ac7f60d915407012e6
Felipe Merchan <felipemerchanfuquen@gmail.com>
2024-10-14 22:34:59 -0500

    25. Paginacion

--- ecommerce/src/pages/Home/Home.tsx
+++ ecommerce/src/pages/Home/Home.tsx
@@ -5,10 +5,23 @@ import { Hero } from "../../components/ui/Hero";
 import styles from "./Home.module.css";
 import { Toaster } from "sonner";
 import { getProducts } from "../../services";
+import { useState } from "react";
 
 export const Home = () => {
-  const { data, isLoading, error } = useQuery("products", getProducts);
+  const [page, setPage] = useState(1);
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["products", page],
+    queryFn: () => getProducts(page),
+    keepPreviousData: true,
+  });
   /* const { products, isLoading, error } = useProducts(); */
+  const handlePreviousPage = () => {
+    setPage((prev) => prev - 1);
+  };
+
+  const handleNextPage = () => {
+    setPage((prev) => prev + 1);
+  };
 
   return (
     <>
@@ -20,6 +33,21 @@ export const Home = () => {
         {data?.map((product) => (
           <CardProduct key={product.id} product={product} />
         ))}
+      </div>
+      <div className={styles.paginationContainer}>
+        <button
+          className={styles.paginationButton}
+          onClick={handlePreviousPage}
+          disabled={page === 1}
+        >
+          Previous page
+        </button>
+        <div>
+          <span className={styles.paginationActive}>{page}</span>
+        </div>
+        <button className={styles.paginationButton} onClick={handleNextPage}>
+          Next page
+        </button>
       </div>
     </>
   );
